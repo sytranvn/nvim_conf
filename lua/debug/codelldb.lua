65e8127489c4ddedc3e0d132b6062316f8f12742
Sy Tran Dung <sytrancsvn@gmail.com>
2024-04-04 23:12:47 +0700

    daplldb-args

--- lua/debug/codelldb.lua
+++ lua/debug/codelldb.lua
@@ -8,8 +8,12 @@ dap.configurations.cpp = {
 			return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
 		end,
 		args = function()
-			local args = vim.fn.input('Args: ')
-			return { args }
+			local args_ = vim.fn.input('Args: ', '', 'file')
+			local args = {}
+			for arg in args_:gmatch("%S+") do
+				table.insert(args, arg)
+			end
+			return args
 		end,
 		cwd = '${workspaceFolder}',
 		stopOnEntry = false,
