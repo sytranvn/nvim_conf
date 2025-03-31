014d570979f124d9ee44bb58d34a930d9f283fe4
Sy Tran <sytrancsvn@gmail.com>
2025-03-31 15:54:13 +0700

    disable markview latex

--- lua/custom/plugins/md.lua
+++ lua/custom/plugins/md.lua
@@ -1,7 +1,7 @@
 return {
     "OXY2DEV/markview.nvim",
-    lazy = false, -- Recommended
-    -- ft = "markdown" -- If you decide to lazy-load anyway
+    lazy = true, -- Recommended
+    ft = "markdown", -- If you decide to lazy-load anyway
 
     dependencies = {
         -- You will not need this if you installed the
@@ -13,34 +13,34 @@ return {
     },
     opts = {
                 latex = {
-                    enable = true,
-                    inline = {
-                        enable = true,
-                    },
-                    operators = {
-                        enable = true,
-                    },
-                    symbols = {
-                        enable = true,
-                        -- symbols not yet in markview
-                        overwrite = {
-                            dots = "…",
-                            quad = "  ",
-                            [" "] = " ",
-                            enspace = " ",
-                            thinspace = "",
-                            iff = "⇔",
-                            leftrightarrows = "⇆",
-                            to = "→",
-                            forall = "∀",
-                            emptyset = "∅",
-                            Chi = "Χ",
-                            choose = "C",
-                            coloneqq = "≔",
-                            E = "E",
-                            Var = "Var",
-                        },
-                    },
+                    enable = false,
+                    -- inline = {
+                    --     enable = true,
+                    -- },
+                    -- operators = {
+                    --     enable = true,
+                    -- },
+                    -- symbols = {
+                    --     enable = true,
+                    --     -- symbols not yet in markview
+                    --     overwrite = {
+                    --         dots = "…",
+                    --         quad = "  ",
+                    --         [" "] = " ",
+                    --         enspace = " ",
+                    --         thinspace = "",
+                    --         iff = "⇔",
+                    --         leftrightarrows = "⇆",
+                    --         to = "→",
+                    --         forall = "∀",
+                    --         emptyset = "∅",
+                    --         Chi = "Χ",
+                    --         choose = "C",
+                    --         coloneqq = "≔",
+                    --         E = "E",
+                    --         Var = "Var",
+                    --     },
+                    -- },
                 }
             }
 }
